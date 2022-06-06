8acf0cb902f1c0c74bff7348002673f9b296e644
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-06 15:20:22 -0300

    Create My Balance Repository test

--- desafio_supremo/test/app/features/infra/repositories/my_balance_repository_impl_test.dart
+++ desafio_supremo/test/app/features/infra/repositories/my_balance_repository_impl_test.dart
@@ -1,3 +1,34 @@
+import 'package:dartz/dartz.dart';
+import 'package:desafio_supremo/app/features/domain/errors/errors.dart';
+import 'package:desafio_supremo/app/features/infra/datasource/my_balance_datasource.dart';
+import 'package:desafio_supremo/app/features/infra/models/balance_model.dart';
+import 'package:desafio_supremo/app/features/infra/repositories/my_balance_repository_impl.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
-void main() {}
+class MyBalanceDatasourceMock extends Mock implements MyBalanceDatasource {}
+
+void main() {
+  final datasource = MyBalanceDatasourceMock();
+  final repository = MyBalanceRepositoryImpl(datasource);
+  var id = faker.guid.guid();
+  final amount = BalanceModel(amount: 1234);
+
+  test("Should return a Balance Model", () async {
+    when(() => datasource.getAmountValue(id)).thenAnswer((_) async => amount);
+
+    final result = await repository.getAmountValue(id);
+
+    expect(result, Right(amount));
+  });
+
+  test("Should return a Datasource Error when dont succeed", () async {
+    when(() => datasource.getAmountValue(id))
+        .thenThrow((_) async => DatasourceError());
+
+    final result = await repository.getAmountValue(id);
+
+    expect(result, Left(DatasourceError()));
+  });
+}
