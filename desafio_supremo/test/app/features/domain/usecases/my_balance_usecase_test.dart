57ec1233c0fc7895780a830b066e1203ba933330
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-06 13:50:51 -0300

    Correct method My Balance Usecase and add Fake package

--- desafio_supremo/test/app/features/domain/usecases/my_balance_usecase_test.dart
+++ desafio_supremo/test/app/features/domain/usecases/my_balance_usecase_test.dart
@@ -3,6 +3,7 @@ import 'package:desafio_supremo/app/features/domain/entities/balance_entity.dart
 import 'package:desafio_supremo/app/features/domain/errors/errors.dart';
 import 'package:desafio_supremo/app/features/domain/repositories/my_balance_repository.dart';
 import 'package:desafio_supremo/app/features/domain/usecases/my_balance_usecase.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -14,24 +15,25 @@ void main() {
   final repository = MyBalanceRepositoryMock();
   final usecase = MyBalanceUsecaseImplementation(repository);
   final amount = BalanceEntityMock();
+  var id = faker.guid.guid();
 
   test('Should return an amount value', () async {
-    when(() => repository.getAmountValue())
+    when(() => repository.getAmountValue(id))
         .thenAnswer((_) async => Right(amount));
 
-    final result = await usecase();
+    final result = await usecase(id);
 
     expect(result, Right(amount));
-    verify(() => repository.getAmountValue()).called(1);
+    verify(() => repository.getAmountValue(id)).called(1);
   });
 
   test('Should return an Server Error when dont succeed', () async {
-    when(() => repository.getAmountValue())
+    when(() => repository.getAmountValue(id))
         .thenAnswer((_) async => Left(ServerError()));
 
-    final result = await usecase();
+    final result = await usecase(id);
 
     expect(result, Left(ServerError()));
-    verify(() => repository.getAmountValue()).called(1);
+    verify(() => repository.getAmountValue(id)).called(1);
   });
 }
