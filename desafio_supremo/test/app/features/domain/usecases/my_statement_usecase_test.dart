e5a3c5d1f37f9d8c5e8221b1ad5890e81ec727b2
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-06 14:43:18 -0300

    Create 'Should return a list of My Statement Entity' test

--- desafio_supremo/test/app/features/domain/usecases/my_statement_usecase_test.dart
+++ desafio_supremo/test/app/features/domain/usecases/my_statement_usecase_test.dart
@@ -1,3 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:desafio_supremo/app/features/domain/entities/statement_entity.dart';
+import 'package:desafio_supremo/app/features/domain/repositories/my_statement_repository.dart';
+import 'package:desafio_supremo/app/features/domain/usecases/my_statement_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
-void main() {}
+class MyStatementRepositoryMock extends Mock implements MyStatementRepository {}
+
+class StatementEntityMock extends Mock implements StatementEntity {}
+
+void main() {
+  final repository = MyStatementRepositoryMock();
+  final usecase = MyStatementUsecaseImpl(repository);
+  final list = [StatementEntityMock()];
+
+  test('Should return a list of My Statement Entity', () async {
+    when(() => repository.getMyStatementsList())
+        .thenAnswer((_) async => Right(list));
+
+    final result = await usecase();
+    expect(result, Right(list));
+    verify(() => repository.getMyStatementsList()).called(1);
+  });
+}
