997e3b9ea405b1469ad9e4a8775d3ce9b6f5b686
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-06 18:08:34 -0300

    Create 'Should throw a Datasource Error if dont succeed' test

--- desafio_supremo/test/app/features/infra/repositories/my_statement_repository_impl_test.dart
+++ desafio_supremo/test/app/features/infra/repositories/my_statement_repository_impl_test.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:desafio_supremo/app/features/domain/errors/errors.dart';
 import 'package:desafio_supremo/app/features/infra/datasource/my_statement_datasource.dart';
 import 'package:desafio_supremo/app/features/infra/models/statement_model/statement_model.dart';
 import 'package:desafio_supremo/app/features/infra/repositories/my_statement_repository_impl.dart';
@@ -26,6 +27,25 @@ void main() {
     final result = await repository.getMyStatementsList();
 
     expect(result, Right(list));
+    verify(() => datasource.getMyStatementsList()).called(1);
+  });
+
+  test('Should throw a Null Error if return a null value', () async {
+    when(() => datasource.getMyStatementsList()).thenAnswer((_) async => null);
+
+    final result = await repository.getMyStatementsList();
+
+    expect(result, Left(NullError()));
+    verify(() => datasource.getMyStatementsList()).called(1);
+  });
+
+  test('Should throw a Datasource Error if dont succeed', () async {
+    when(() => datasource.getMyStatementsList())
+        .thenThrow((_) async => DatasourceError());
+
+    final result = await repository.getMyStatementsList();
+
+    expect(result, Left(DatasourceError()));
     verify(() => datasource.getMyStatementsList()).called(1);
   });
 }
