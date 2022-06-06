e240decad47131c99e05a3dee5bb4a8a410f7bff
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-06 12:04:14 -0300

    Create MyBalanceRepositoryMock

--- desafio_supremo/test/app/features/domain/usecases/my_balance_usecase_test.dart
+++ desafio_supremo/test/app/features/domain/usecases/my_balance_usecase_test.dart
@@ -1,7 +1,12 @@
+import 'package:desafio_supremo/app/features/domain/repositories/my_balance_repository.dart';
 import 'package:desafio_supremo/app/features/domain/usecases/my_balance_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
+class MyBalanceRepositoryMock extends Mock implements MyBalanceRepository {}
+
 void main() {
+  final repository = MyBalanceRepositoryMock();
+  final usecase = MyBalanceUsecaseImplementation(repository);
   test('Should return a amount value', () {});
 }
