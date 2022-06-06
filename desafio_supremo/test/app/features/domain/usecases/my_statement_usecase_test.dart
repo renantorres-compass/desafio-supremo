2a920f8a6e35fbd6561453885f7d90a1556812a7
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-06 14:47:04 -0300

    Update my_statement_usecase_test.dart

--- desafio_supremo/test/app/features/domain/usecases/my_statement_usecase_test.dart
+++ desafio_supremo/test/app/features/domain/usecases/my_statement_usecase_test.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:desafio_supremo/app/features/domain/entities/statement_entity.dart';
+import 'package:desafio_supremo/app/features/domain/errors/errors.dart';
 import 'package:desafio_supremo/app/features/domain/repositories/my_statement_repository.dart';
 import 'package:desafio_supremo/app/features/domain/usecases/my_statement_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -20,6 +21,15 @@ void main() {
 
     final result = await usecase();
     expect(result, Right(list));
+    verify(() => repository.getMyStatementsList()).called(1);
+  });
+
+  test('Should return an Server Error when dont succeed', () async {
+    when(() => repository.getMyStatementsList())
+        .thenAnswer((_) async => Left(ServerError()));
+
+    final result = await usecase();
+    expect(result, Left(ServerError()));
     verify(() => repository.getMyStatementsList()).called(1);
   });
 }
