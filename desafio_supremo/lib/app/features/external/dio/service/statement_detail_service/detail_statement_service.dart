f57aac0547fe4d8152e9b92ca3ece692557e68f9
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-07 18:38:55 -0300

    Create a 'Should return a Detail Statement model' test

--- desafio_supremo/lib/app/features/external/dio/service/statement_detail_service/detail_statement_service.dart
+++ desafio_supremo/lib/app/features/external/dio/service/statement_detail_service/detail_statement_service.dart
@@ -3,8 +3,6 @@ import 'package:desafio_supremo/app/features/infra/models/detail_statement_model
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
 
-import '../../../../infra/models/balance_model/balance_model.dart';
-
 part 'detail_statement_service.g.dart';
 
 @RestApi(baseUrl: Api.baseUrl)
--- desafio_supremo/test/app/features/external/dio/datasource/detail_statement_dio_datasource_test.dart
+++ desafio_supremo/test/app/features/external/dio/datasource/detail_statement_dio_datasource_test.dart
@@ -1,3 +1,32 @@
+import 'package:desafio_supremo/app/core/constants/constants.dart';
+import 'package:desafio_supremo/app/features/external/dio/datasource/detail_statement_dio_datasource.dart';
+import 'package:desafio_supremo/app/features/external/dio/service/statement_detail_service/detail_statement_service.dart';
+import 'package:desafio_supremo/app/features/infra/models/detail_statement_model/detail_statement_model.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
-void main() {}
+class DetailStatementServiceMock extends Mock
+    implements DetailStatementService {}
+
+void main() {
+  final service = DetailStatementServiceMock();
+  final datasource = DetailStatementDioDatasource(service);
+  final id = faker.guid.guid();
+  final detailStatement = DetailStatementModel(
+      amount: 1234,
+      id: id,
+      authentication: 'authentication',
+      tType: 'tType',
+      createdAt: 'createdAt',
+      to: 'to',
+      description: 'description');
+
+  test('Should return a Detail Statement model', () async {
+    when(() => service.getStatementDetail(Auth.tokenValue, id))
+        .thenAnswer((_) async => detailStatement);
+    final result = await datasource.getDetailStatement(id);
+    expect(result, detailStatement);
+    verify(() => service.getStatementDetail(Auth.tokenValue, id)).called(1);
+  });
+}
