cccc9cc39facc17e621c90c22bda9aafcae2fd4b
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-08 14:22:09 -0300

    Create My Balance Bloc tests

--- desafio_supremo/lib/app/features/presenter/bloc/my_balance/my_balance_state.dart
+++ desafio_supremo/lib/app/features/presenter/bloc/my_balance/my_balance_state.dart
@@ -1,19 +1,30 @@
 import 'package:desafio_supremo/app/features/infra/models/balance_model/balance_model.dart';
+import 'package:equatable/equatable.dart';
 
-abstract class MyBalanceState {
-  BalanceModel? balance;
+abstract class MyBalanceState extends Equatable {
+  final BalanceModel? balance;
 
-  MyBalanceState({required this.balance});
+  const MyBalanceState({required this.balance});
 }
 
 class MyBalanceInitialState extends MyBalanceState {
   MyBalanceInitialState() : super(balance: BalanceModel(amount: 0));
+
+  @override
+  List<Object?> get props => [];
 }
 
 class MyBalanceSuccessState extends MyBalanceState {
-  MyBalanceSuccessState({required super.balance});
+  const MyBalanceSuccessState({required BalanceModel balance})
+      : super(balance: balance);
+
+  @override
+  List<Object?> get props => [balance];
 }
 
 class MyBalanceErrorState extends MyBalanceState {
-  MyBalanceErrorState() : super(balance: null);
+  const MyBalanceErrorState() : super(balance: null);
+
+  @override
+  List<Object?> get props => [];
 }
--- desafio_supremo/test/app/features/presenter/bloc/my_balance/my_balance_bloc_test.dart
+++ desafio_supremo/test/app/features/presenter/bloc/my_balance/my_balance_bloc_test.dart
@@ -1,6 +1,64 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:desafio_supremo/app/features/infra/models/balance_model/balance_model.dart';
 import 'package:desafio_supremo/app/features/presenter/bloc/my_balance/my_balance_bloc.dart';
+import 'package:desafio_supremo/app/features/presenter/bloc/my_balance/my_balance_events.dart';
+import 'package:desafio_supremo/app/features/presenter/bloc/my_balance/my_balance_state.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+class MyBalanceBlocMock extends MockBloc<MyBalanceEvents, MyBalanceState>
+    implements MyBalanceBloc {}
+
 void main() {
-  final myBalanceBloc = MyBalanceBloc(usecase);
+  MyBalanceBloc bloc = MyBalanceBlocMock();
+  BalanceModel balance =
+      BalanceModel(amount: faker.randomGenerator.integer(1000));
+
+  final expectedSuccess = [
+    MyBalanceInitialState(),
+    MyBalanceSuccessState(balance: balance),
+  ];
+
+  final expectedError = [MyBalanceInitialState(), const MyBalanceErrorState()];
+
+  test('Initial state is correct', () async {
+    whenListen(bloc, Stream<MyBalanceState>.fromIterable(expectedSuccess),
+        initialState: MyBalanceInitialState());
+    expect(bloc.state, MyBalanceInitialState());
+    bloc.close();
+  });
+
+  test('Emits My Balance Success State when succeed', () async {
+    whenListen(bloc, Stream<MyBalanceState>.fromIterable(expectedSuccess),
+        initialState: MyBalanceInitialState());
+
+    expect(bloc.state, equals(MyBalanceInitialState()));
+    bloc.add(LoadMyBalanceEvents());
+    expectLater(
+        bloc.stream,
+        emitsInOrder(
+          [
+            MyBalanceInitialState(),
+            MyBalanceSuccessState(balance: balance),
+            emitsDone
+          ],
+        ));
+
+    bloc.close();
+  });
+
+  test('Emits My Balance Error State when dont succeed', () async {
+    whenListen(bloc, Stream<MyBalanceState>.fromIterable(expectedError),
+        initialState: MyBalanceInitialState());
+
+    expect(bloc.state, equals(MyBalanceInitialState()));
+    bloc.add(LoadMyBalanceEvents());
+    expectLater(
+        bloc.stream,
+        emitsInOrder(
+          [MyBalanceInitialState(), const MyBalanceErrorState(), emitsDone],
+        ));
+
+    bloc.close();
+  });
 }
