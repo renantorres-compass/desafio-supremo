e0f6982f28e57e00c6348ee145f67ccde632c739
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-08 14:51:11 -0300

    Create My Statement States

--- desafio_supremo/lib/app/features/presenter/bloc/my_balance/my_balance_state.dart
+++ desafio_supremo/lib/app/features/presenter/bloc/my_balance/my_balance_state.dart
@@ -26,5 +26,5 @@ class MyBalanceErrorState extends MyBalanceState {
   const MyBalanceErrorState() : super(balance: null);
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [balance];
 }
--- desafio_supremo/lib/app/features/presenter/bloc/my_statement/my_statement_state.dart
+++ desafio_supremo/lib/app/features/presenter/bloc/my_statement/my_statement_state.dart
@@ -1,1 +1,30 @@
+import 'package:equatable/equatable.dart';
 
+import '../../../infra/models/statement_items_model/statement_items_model.dart';
+
+abstract class MyStatementState extends Equatable {
+  final StatementItemsModel? items;
+
+  const MyStatementState({required this.items});
+}
+
+class MyStatementInitialState extends MyStatementState {
+  MyStatementInitialState() : super(items: StatementItemsModel(items: []));
+
+  @override
+  List<Object?> get props => [];
+}
+
+class MyStatementSuccessState extends MyStatementState {
+  const MyStatementSuccessState({required super.items});
+
+  @override
+  List<Object?> get props => [items];
+}
+
+class MyStatementErrorState extends MyStatementState {
+  const MyStatementErrorState() : super(items: null);
+
+  @override
+  List<Object?> get props => [items];
+}
