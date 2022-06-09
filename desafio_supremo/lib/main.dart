0fc88cfb968ba5bb52f6ce32c7c9b00cdb1f7bf6
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-09 16:21:39 -0300

    Add DependencyInjection to main

--- desafio_supremo/lib/main.dart
+++ desafio_supremo/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:desafio_supremo/app/app_widget.dart';
+import 'package:desafio_supremo/app/core/utils/dependency_injection.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DependencyInjection.init();
   runApp(const AppWidget());
 }
