ec3a992efb22b4c2d36651fd20a2fc062fd3c3ba
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-10 14:08:27 -0300

    Create details_page.dart

---
+++ desafio_supremo/lib/app/features/presenter/pages/details_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatefulWidget {
+  DetailsPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SizedBox(),
+    );
+  }
+}
