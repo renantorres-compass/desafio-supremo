5038a8d4b2e645cea5d8119939a326833e15c977
Renan Campos Torres <106977020+renantorres-compass@users.noreply.github.com>
2022-06-15 11:06:15 -0300

    Update custom_details_list.dart

--- desafio_supremo/lib/app/features/presenter/widgets/details_widgets/custom_details_list.dart
+++ desafio_supremo/lib/app/features/presenter/widgets/details_widgets/custom_details_list.dart
@@ -28,7 +28,6 @@ class CustomDetailsList extends StatelessWidget {
         child: model != null
             ? ListView.builder(
                 shrinkWrap: true,
-                itemExtent: 10,
                 itemCount: list.length,
                 itemBuilder: (context, index) {
                   return Padding(
